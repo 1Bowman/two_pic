76c2aedd4c0eed4d99ad78f61b4b40ffa7a5a128
Bowman <onlyonebowman@gmail.com>
2016-06-23 18:30:58 -0500

    Made a sqlite database with sql-migrate

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 from app import create_app, db
 from app.main.models import Post
 from flask_script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
