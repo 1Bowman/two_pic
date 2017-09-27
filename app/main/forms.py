990bafb988e787eae83ee9322ca9a3c742a65196
Alex Bowman <onlyonebowman@gmail.com>
2017-09-27 11:46:57 -0500

    Changed the length of description field

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,7 +7,7 @@ class UploadForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 64)])
     first_name = StringField('First Name', validators=[DataRequired(), Length(1, 32)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(1, 32)])
-    description = TextAreaField('Description of Image', validators=[Length(0, 140)])
+    description = TextAreaField('Description of Image', validators=[Length(0, 141)])
     image = FileField('Image', validators=[DataRequired()])
 
     submit = SubmitField()
